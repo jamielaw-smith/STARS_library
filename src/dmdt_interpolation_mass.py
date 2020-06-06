2957c8ea8b77e50c09be8442b51ac155586c8a0d
Dave Coulter <davecoulter@enceladus.mhs>
2020-06-06 13:35:39 -0700

    fixing directory bug

--- src/dmdt_interpolation_mass.py
+++ src/dmdt_interpolation_mass.py
@@ -27,8 +27,9 @@ Mhbase = 1.0e6 * M_SUN_CGS  # this is the generic size of bh used
 NUM_MASS_INTERP_POINTS = 10
 
 model_directories_by_time = {
-    "t0.0": ["m0.3", "m0.5", "m0.7", "m1.0", "m3.0"],
-    "t1.0": ["m0.3", "m0.5", "m0.7", "m1.0", "m3.0"],
+    "t0.0": ["m1.0", "m3.0"],
+    # "t0.0": ["m0.3", "m0.5", "m0.7", "m1.0", "m3.0"],
+    # "t1.0": ["m0.3", "m0.5", "m0.7", "m1.0", "m3.0"],
 }
 
 model_dir_formatter = "{}_{}"
@@ -44,7 +45,7 @@ for age_string, mass_steps in  model_directories_by_time.items():
         print("Processing: [%s, %s] for time %s" % (m1, m2, age_string))
 
         m1_dir = model_dir_formatter.format(m1, age_string)
-        m2_dir = model_dir_formatter.format(m1, age_string)
+        m2_dir = model_dir_formatter.format(m2, age_string)
         dmdt_sub_dirs = [m1_dir, m2_dir]
 
         # --------- GET SIMULATION BETAS -----------------
