ff4c3fff74de14c9bb7c8ca268c38ced1abdd1a5
Dave Coulter <davecoulter@enceladus.mhs>
2020-06-06 13:03:37 -0700

    automating mass script

--- src/dmdt_interpolation_mass.py
+++ src/dmdt_interpolation_mass.py
@@ -2,7 +2,6 @@
 copied from _1param.py
 interpolating in mass, mapping betas in between masses, keeping age constant
 replaced _beta with _mass often
-
 todo think about what to do for mdots past critical beta
 todo right now it looks like works best when only run between two masses at a time
 """
@@ -27,62 +26,46 @@ Mhbase = 1.0e6 * M_SUN_CGS  # this is the generic size of bh used
 # USER INPUTS
 NUM_MASS_INTERP_POINTS = 10
 
-intput_dir = '../input/'
-output_dir = '../output/'
-
 model_directories_by_time = {
-    ("t0.0", "t0.0"): ["m0.3", "m0.5", "m1.0", "m3.0"],
-    ("t1.0", "t1.0"): ["m0.3", "m0.5", "m0.7", "m1.0", "m3.0"],
+    "t0.0": ["m0.3", "m0.5", "m1.0", "m3.0"],
+    "t1.0": ["m0.3", "m0.5", "m0.7", "m1.0", "m3.0"],
 }
 
-# DC scratch...
-    # m0.7/t10.0 -> m1.0/t8.4
-        # age_string == t10.0, t9.84, t9.68, ... t8.4
-        # mass_string == m0.7, m0.73, m0.76 ... m1.0
-
-    # m1.0/t8.4 -> m3.0/t0.3
-
-
-output_dir_formatter = "{}{}"
 model_dir_formatter = "{}_{}"
-beta_file_formatter = "{}/{}"
-interp_output_subdir_formatter = "m{}_{}/"
 
-for t, mass_list in model_directories_by_time.items():
+dmdt_input_dir = '../input/'
+output_dir = '../output/'
 
-    age_strings = []
-    t1 = t[0]
-    t2 = t[1]
-    if t1 == t2: # initialize to same value
-        for m in mass_list:
-            age_strings.append(t1)
-    else: # initialize by linear increments
-        t1_num = float(t1[1:])
-        t2_num = float(t2[1:])
-        t_steps = np.linspace(t1_num, t2_num, NUM_MASS_INTERP_POINTS)
-        for t_step in t_steps:
-            age_strings.append("t{:0.1f}".format(t_step))
 
-    for m1, m2 in zip(mass_list[:-1], mass_list[1:]):
+for age_string, mass_steps in  model_directories_by_time.items():
 
-        print("Processing: [%s, %s] for time array %s" % (m1, m2, age_strings))
-        mass_range_strings = [m1, m2]
+    for m1, m2 in zip(mass_steps[:-1], mass_steps[1:]):
 
-        # Do process for m1
-        # --------- GET INTERPOLATED BETAS -----------------
-        m1_output_subdir = output_dir_formatter.format(output_dir, model_dir_formatter.format(m1, t1))
-        a = [f for f in os.listdir(m1_output_subdir) if not f.startswith('.')]
+        print("Processing: [%s, %s] for time %s" % (m1, m2, age_string))
+
+        m1_dir = model_dir_formatter.format(m1, age_string)
+        m2_dir = model_dir_formatter.format(m1, age_string)
+        dmdt_sub_dirs = [m1_dir, m2_dir]
+
+        # --------- GET SIMULATION BETAS -----------------
+        lo_sim_beta_files = os.listdir(dmdt_input_dir + dmdt_sub_dirs[0])
+        lo_sim_beta_files.sort()
+        lo_sim_betas = [float(b[:-4]) for b in lo_sim_beta_files]
+
+        # find lo_interp_beta_files
+        a = [f for f in os.listdir(output_dir + dmdt_sub_dirs[0]) if not f.startswith('.')]
         a.sort()
 
+        # this will select 11 betas from lowest to highest. spaced by log from logsapce
         lo_interp_beta_files = a[::10]
         lo_interp_beta_files.append(a[-1])
         lo_interp_betas = [float(b[:-4]) for b in lo_interp_beta_files]
 
-        Sim_mass = [float(f[1:4]) for f in mass_range_strings]
-        mass_arr = np.logspace(np.log10(Sim_mass[0]), np.log10(Sim_mass[-1]), num=NUM_MASS_INTERP_POINTS)
+
+        Sim_mass = [float(m1[1:4]), float(m2[1:4])]
+        mass_arr = np.linspace(Sim_mass[0], Sim_mass[-1], num=NUM_MASS_INTERP_POINTS)
 
         for z, low_interp_beta_file in enumerate(lo_interp_beta_files):
-
             # ------ DIRECTORY PARAMETERS -------
 
             # It is assumed that there are different files for each beta
@@ -92,12 +75,13 @@ for t, mass_list in model_directories_by_time.items():
             # dictionaries with gamma's as keys.
             Beta_slope = [] # {gammas[0]: [], gammas[1]: []}
             Beta_yinter = [] # {gammas[0]: [], gammas[1]: []}
-            Mapped_time = [] # {gammas[0]: [], gammas[1]: []}
 
+            Mapped_time = [] # {gammas[0]: [], gammas[1]: []}
             # for converting back from mapped time to actual times and doing
             # interpolation in actual time
             Premaptime = [] #{gammas[0]: [], gammas[1]: []}
             Premapdmdt = [] #{gammas[0]: [], gammas[1]: []}
+
 
             # ----- CREATE INTERPOLATION FUNCTIONS; FIND SLOPES & YINTERs -----
             time = {}
@@ -106,9 +90,7 @@ for t, mass_list in model_directories_by_time.items():
             mapped_time = {}
             # get dmdt and t for the lowest beta value
             # energy & dmde (cgs)
-            time['lo'], dmdt['lo'] = np.genfromtxt(beta_file_formatter.format(m1_output_subdir, low_interp_beta_file),
-                                                   skip_header=1,
-                                                   unpack=True)
+            time['lo'], dmdt['lo'] = np.genfromtxt(output_dir + dmdt_sub_dirs[0] + "/" + low_interp_beta_file, skip_header=1, unpack=True)
             ipeak['lo'] = np.argmax(dmdt['lo'])
 
             # split time['lo'] & dmdt['lo'] into pre-peak and post-peak array
@@ -125,100 +107,95 @@ for t, mass_list in model_directories_by_time.items():
             Premaptime.append(np.copy(time['lo']))
             Premapdmdt.append(np.copy(dmdt['lo']))
 
+            #for i in range(1, len(Sim_beta)):
+            for i in range(1, len(Sim_mass)):
+                a = [f for f in os.listdir(output_dir + dmdt_sub_dirs[i]) if not f.startswith('.')]
+                a.sort()
+                # this will select 11 betas from lowest to highest. spaced by log from logsapce
+                hi_interp_beta_files = a[::10]
+                hi_interp_beta_files.append(a[-1])
+                hi_interp_betas = [float(b[:-4]) for b in hi_interp_beta_files]
 
 
-            # Do same process for m2
-            m2_output_subdir = output_dir_formatter.format(output_dir, model_dir_formatter.format(m2, t2))
-            a = [f for f in os.listdir(m2_output_subdir) if not f.startswith('.')]
-            a.sort()
-            # this will select 11 betas from lowest to highest. spaced by log from logsapce
-            hi_interp_beta_files = a[::10]
-            hi_interp_beta_files.append(a[-1])
-            hi_interp_betas = [float(b[:-4]) for b in hi_interp_beta_files]
+                if len(hi_interp_beta_files) != len(lo_interp_beta_files):
+                    print('ERROR not same number of betas. Not sure what to do?')
 
-            if len(hi_interp_beta_files) != len(lo_interp_beta_files):
-                print('ERROR not same number of betas. Not sure what to do?')
+                time['hi'], dmdt['hi'] = np.genfromtxt(output_dir + dmdt_sub_dirs[i] + "/" + hi_interp_beta_files[z], skip_header=1, unpack=True)
+                ipeak['hi'] = np.argmax(dmdt['hi'])
 
-            time['hi'], dmdt['hi'] = np.genfromtxt(beta_file_formatter.format(m2_output_subdir,
-                                                                              hi_interp_beta_files[z]),
-                                                   skip_header=1,
-                                                   unpack=True)
+                # split time_hi and dmdt_hi into pre-peak and post-peak array
+                # peak in 2nd array
+                time['hi'] = np.array([time['hi'][:ipeak['hi']],
+                                       time['hi'][ipeak['hi']:]])
+                dmdt['hi'] = np.array([dmdt['hi'][:ipeak['hi']],
+                                       dmdt['hi'][ipeak['hi']:]])
+                # will contain time/dmdt arrays
+                # (split into pre & post peak times/dmdts)
+                # for each beta value
+                Premapdmdt.append(np.copy(dmdt['hi']))
+                Premaptime.append(np.copy(time['hi']))
 
-            ipeak['hi'] = np.argmax(dmdt['hi'])
+                mapped_time['hi'] = []
+                mapped_time['lo'] = []
 
-            # split time_hi and dmdt_hi into pre-peak and post-peak array
-            # peak in 2nd array
-            time['hi'] = np.array([time['hi'][:ipeak['hi']],
-                                   time['hi'][ipeak['hi']:]])
-            dmdt['hi'] = np.array([dmdt['hi'][:ipeak['hi']],
-                                   dmdt['hi'][ipeak['hi']:]])
-            # will contain time/dmdt arrays
-            # (split into pre & post peak times/dmdts)
-            # for each beta value
-            Premapdmdt.append(np.copy(dmdt['hi']))
-            Premaptime.append(np.copy(time['hi']))
+                Beta_slope.append([])
 
-            mapped_time['hi'] = []
-            mapped_time['lo'] = []
+                Beta_yinter.append([])
+                Mapped_time.append([])
+                for j in [0, 1]:  # once before peak, once after peak
+                    # choose more densely sampled curve to map times to 0-1
+                    # less densely sampled curve will be interpolated to match
+                    if len(time['lo'][j]) < len(time['hi'][j]):
+                        # hi array more densely sampled
+                        interp = 'lo'
+                        nointerp = 'hi'
+                    else:
+                        # will also catch case where they have the same lengths
+                        interp = 'hi'
+                        nointerp = 'lo'
+                    # map times from more densely sampled curves
+                    # (both pre & post peak, might be from diff. dmdts)
+                    # to 0 - 1
+                    mapped_time[nointerp].append(
+                        1. / (time[nointerp][j][-1] - time[nointerp][j][0]) *
+                        (time[nointerp][j] - time[nointerp][j][0]))
+                    mapped_time[interp].append(
+                        1. / (time[interp][j][-1] - time[interp][j][0]) *
+                        (time[interp][j] - time[interp][j][0]))
 
-            Beta_slope.append([])
+                    # ensure bounds are same for interp and nointerp
+                    # before interpolation
+                    # (should be 0 and 1 from above, but could be slightly off
+                    # due to rounding errors in python)
+                    mapped_time[interp][j][0] = 0
+                    mapped_time[interp][j][-1] = 1
+                    mapped_time[nointerp][j][0] = 0
+                    mapped_time[nointerp][j][-1] = 1
 
-            Beta_yinter.append([])
-            Mapped_time.append([])
-            for j in [0, 1]:  # once before peak, once after peak
-                # choose more densely sampled curve to map times to 0-1
-                # less densely sampled curve will be interpolated to match
-                if len(time['lo'][j]) < len(time['hi'][j]):
-                    # hi array more densely sampled
-                    interp = 'lo'
-                    nointerp = 'hi'
-                else:
-                    # will also catch case where they have the same lengths
-                    interp = 'hi'
-                    nointerp = 'lo'
-                # map times from more densely sampled curves
-                # (both pre & post peak, might be from diff. dmdts)
-                # to 0 - 1
-                mapped_time[nointerp].append(
-                    1. / (time[nointerp][j][-1] - time[nointerp][j][0]) *
-                    (time[nointerp][j] - time[nointerp][j][0]))
-                mapped_time[interp].append(
-                    1. / (time[interp][j][-1] - time[interp][j][0]) *
-                    (time[interp][j] - time[interp][j][0]))
+                    func = interp1d(mapped_time[interp][j], dmdt[interp][j])
+                    dmdtinterp = func(mapped_time[nointerp][j])
 
-                # ensure bounds are same for interp and nointerp
-                # before interpolation
-                # (should be 0 and 1 from above, but could be slightly off
-                # due to rounding errors in python)
-                mapped_time[interp][j][0] = 0
-                mapped_time[interp][j][-1] = 1
-                mapped_time[nointerp][j][0] = 0
-                mapped_time[nointerp][j][-1] = 1
+                    if interp == 'hi':
+                        slope = ((dmdtinterp - dmdt['lo'][j]) / (Sim_mass[i] - Sim_mass[i - 1]))
+                    else:
+                        slope = ((dmdt['hi'][j] - dmdtinterp) / (Sim_mass[i] - Sim_mass[i - 1]))
 
-                func = interp1d(mapped_time[interp][j], dmdt[interp][j])
-                dmdtinterp = func(mapped_time[nointerp][j])
+                    # this is really Mass_slope now?
+                    Beta_slope[-1].append(slope)
 
-                if interp == 'hi':
-                    # todo probably need to edit this for mass
-                    slope = ((dmdtinterp - dmdt['lo'][j]) / (Sim_mass[1] - Sim_mass[0]))
-                else:
-                    slope = ((dmdt['hi'][j] - dmdtinterp) / (Sim_mass[1] - Sim_mass[0]))
+                    yinter1 = (dmdt[nointerp][j] - Beta_slope[-1][j] * Sim_mass[i - 1])
+                    yinter2 = (dmdtinterp - Beta_slope[-1][j] * Sim_mass[i])
+                    Beta_yinter[-1].append((yinter1 + yinter2) / 2.0)
+                    Mapped_time[-1].append(np.array(mapped_time[nointerp][j]))
 
-                # this is really Mass_slope now?
-                Beta_slope[-1].append(slope)
+                time['lo'] = np.copy(time['hi'])
+                dmdt['lo'] = np.copy(dmdt['hi'])
 
-                yinter1 = (dmdt[nointerp][j] - Beta_slope[-1][j] * Sim_mass[0])
-                yinter2 = (dmdtinterp - Beta_slope[-1][j] * Sim_mass[1])
-                Beta_yinter[-1].append((yinter1 + yinter2) / 2.0)
-                Mapped_time[-1].append(np.array(mapped_time[nointerp][j]))
-
-            time['lo'] = np.copy(time['hi'])
-            dmdt['lo'] = np.copy(dmdt['hi'])
 
             interp_index_low = [0 for i in mass_arr]
             interp_index_high = [0 for i in mass_arr]
 
-            for i, b in enumerate(mass_arr):
+            for i, b in enumerate(mass_arr):#range(len(beta_arr)):
                 for j in range(len(Sim_mass)):
                     if b == Sim_mass[j]:
                         # no need to interp, already have dmdt & t for this beta
@@ -233,15 +210,12 @@ for t, mass_list in model_directories_by_time.items():
                         beta_interp = True
                         break
 
-            # DC: Make combinations of mX and tX --
-            #   tX will be the same value if there's only one time step for the models
-            for i, age_string in zip(range(len(mass_arr)), age_strings):
+            for i in range(len(mass_arr)):
                 if interp_index_high[i] == interp_index_low[i]:
                     # already have this mass
                     # probably already wrote this mass directory in interpolated_dmdts directory
                     pass
                 else:
-
                     dmdtinterpolated = np.array([
                                         Beta_yinter[interp_index_low[i]][0] +
                                         Beta_slope[interp_index_low[i]][0] * mass_arr[i],
@@ -276,23 +250,22 @@ for t, mass_list in model_directories_by_time.items():
 
                     timeinterpolated = np.array(timeinterpolated)
 
-                    interp_output_subdir = output_dir_formatter.format(output_dir,
-                        interp_output_subdir_formatter.format(str(round(mass_arr[i], 3))[:5], age_string))
+                    savesmalldir = 'm' + str(round(mass_arr[i], 3))[:5] + '_' + age_string + '/'
+                    if not os.path.exists(output_dir + savesmalldir):
+                        os.makedirs(output_dir + savesmalldir)
 
-                    if not os.path.exists(interp_output_subdir):
-                        os.makedirs(interp_output_subdir)
 
                     # todo maybe need to assign beta string earlier above?
-                    beta_float = round(lo_interp_betas[z] +
-                                       ((mass_arr[i] - Sim_mass[0])/(Sim_mass[-1] - Sim_mass[0])) *
-                                       (hi_interp_betas[z] - lo_interp_betas[z]), 3)
+                    beta_float = round(lo_interp_betas[z] + ((mass_arr[i] - Sim_mass[0])/(Sim_mass[-1] - Sim_mass[0]))\
+                                  * (hi_interp_betas[z] - lo_interp_betas[z]),3)
 
                     beta_string = '{0:f}'.format(beta_float)[:5]
-                    file_to_save = interp_output_subdir + beta_string + '.dat'
+
+                    file_to_save = output_dir + savesmalldir + beta_string + '.dat'
                     print("\tSaving %s" % file_to_save)
                     np.savetxt(file_to_save,
                                np.transpose([np.concatenate([timeinterpolated[0], timeinterpolated[1]]),
-                                             np.concatenate([dmdtinterpolated[0], dmdtinterpolated[1]])]))
+                            np.concatenate([dmdtinterpolated[0], dmdtinterpolated[1]])]))
 
 stop_time = tm.time()
 duration = stop_time - start_time
